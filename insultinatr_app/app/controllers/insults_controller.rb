bd9e5704b926dee376f839c962df96c46f3a191f
bubbagump2020 <bubbagump2020@gmail.com>
2019-09-10 17:25:25 -0500

    Getting the insult generator going. Huzzah

--- insultinatr_app/app/controllers/insults_controller.rb
+++ insultinatr_app/app/controllers/insults_controller.rb
@@ -1,5 +1,25 @@
 class InsultsController < ActionController::Base
     
-    
+    def index
+        @insults = Insult.all
+        # byebug
+    end
+
+    def new
+        # byebug
+        @user_id = session[:current_user_id]
+        @insult = Insult.new
+    end
+
+    def create
+        # byebug
+        swear_word = Swear.all.sample
+        @insult = Insult.create({
+            user_id: session[:current_user_id],
+            insult: swear_word.word
+        })
+        redirect_to("/insults")
+        # byebug
+    end
     
 end
