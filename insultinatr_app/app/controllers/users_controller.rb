ce48f9625c51299534ba1ef55c75604c3f243ffb
bubbagump2020 <bubbagump2020@gmail.com>
2019-09-10 12:15:17 -0500

    Got the database and html working together.

--- insultinatr_app/app/controllers/users_controller.rb
+++ insultinatr_app/app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ActionController::Base
 
     def create
+        @user = User.create(user_params)
+        redirect_to "/users/#{@user.id}"
+    end
+
+    def show
 
     end
 
     def user_params
-        params.require(:user).permit(:user_name, :password)
+        params.require(:user).permit(:username, :password)
     end
 
 end
