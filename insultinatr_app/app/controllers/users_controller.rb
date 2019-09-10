27329ff0799ad94eeb39260f169f9faf816f4e9e
bubbagump2020 <bubbagump2020@gmail.com>
2019-09-10 14:58:54 -0500

    Getting login and account creation to work

--- insultinatr_app/app/controllers/users_controller.rb
+++ insultinatr_app/app/controllers/users_controller.rb
@@ -1,25 +1,28 @@
 class UsersController < ActionController::Base
 
+    def index
+        @user = User.find_by(params[:id])
+        redirect_to("/users/#{@user.id}")
+    end
+    # the instance variable in the new method is needed for the create method
+    def new
+        @user = User.new
+    end
+
     def create
         @user = User.create(user_params)
-        redirect_to "/users/#{@user.id}"
-    end
-
-    def show
-
-    end
-
-    def user_params
-        params.require(:user).permit(:username, :password)
-
-        })
         redirect_to("/users/#{@user.id}")
     end
 
-    def show
-        @user = User.find_by({ id:params[:id]})
-        @insults = Insult.all
 
+    def user_params
+        # byebug
+        params.require(:user).permit( :email, :password, :password_confirmation)
+    end
+
+    def show
+        @user = User.find_by({ id: params[:id]})
+        @insults = Insult.all
     end
 
 end
--- insultinatr_app/app/models/User.rb
+++ insultinatr_app/app/models/User.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many(:insults)
     has_many(:names)
+    #validates :email, presence: true uniqueness: true
     validates :password, confirmation: true
     has_secure_password
 end
