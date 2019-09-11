394c339af00a9c5951de77f8e078b7d1729751b7
bubbagump2020 <bubbagump2020@gmail.com>
2019-09-11 17:59:17 -0500

    Fixing the Logout functionality. Next up, name generator.

--- insultinatr_app/app/controllers/users_controller.rb
+++ insultinatr_app/app/controllers/users_controller.rb
@@ -1,29 +1,27 @@
 class UsersController < ActionController::Base
-    # Logic: Index is supposed to show a list of all users
-    # instead I have it find the logged in user and redirect
-    # them to the their own page
-    def index
-        @user = User.find_by(params[:id])
-        redirect_to("/users/#{@user.id}")
+    def show
+        @swears = Swear.all
+        @user = User.find(params[:id])
     end
-    # the instance variable in the new method is needed for the create method
+
     def new
         @user = User.new
     end
 
     def create
         @user = User.create(user_params)
-        redirect_to("/users/#{@user.id}")
+        if @user.save
+            flash[:success] = "Welcome!"
+            redirect_to @user
+        else
+            render 'new'
+        end
+
     end
 
-
-    def user_params
-        params.require(:user).permit( :email, :password, :password_confirmation)
-    end
-
-    def show
-        @user = User.find_by({ id: params[:id]})
-        @swears = Swear.all
-    end
+    private
+        def user_params
+            params.require(:user).permit( :email, :password, :password_confirmation)
+        end
 
 end
