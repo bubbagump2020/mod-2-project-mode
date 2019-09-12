4cc3576d94ebb3bf9ef90ac9aa5a01dbb50c571b
bubbagump2020 <bubbagump2020@gmail.com>
2019-09-12 15:33:16 -0500

    Getting Ahoy and Rails-Social-Share to work.

--- insultinatr_app/app/controllers/application_controller.rb
+++ insultinatr_app/app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
     skip_before_action(:verify_authenticity_token)
     include SessionsHelper
+    after_action :track_action
+
+    protected
+
+    def track_action
+        ahoy.track "Ran action", request.path_parameters
+    end
     # before_action(:check_for_authentication)
 
     # def check_for_authentication
